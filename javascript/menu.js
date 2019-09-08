1644275ae63cdf96fb2ebfc138d1919f452423db
Ruddy Galo <galope_03@hotmail.com>
2019-09-08 12:14:12 -0600

    uploading local to prod

---
+++ javascript/menu.js
@@ -0,0 +1,7 @@
+function click_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function click_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
